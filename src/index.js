0560d9b2bb545d2f7e76701b4292120911ab1cc5
Muhammad Khalid <muhammadkhalid.ch129@gmail.com>
2021-01-18 23:15:36 +0500

    JSX form

--- src/index.js
+++ src/index.js
@@ -1,4 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<h1>Hello world <b>I am Developer</b></h1>, document.getElementById('root'))
+ReactDOM.render(<React.Fragment>
+    <h1>City Name</h1>
+    <ul>
+        <li>Karachi</li>
+        <li>Lahore</li>
+        <li>Islamabad</li>
+    </ul>
+</React.Fragment>, document.getElementById('root'))
+
+
+// Alternative of React Element
+
+// var h1 = document.createElement("h1");
+// h1.innerHTML = "Hello World";
+// document.getElementById("root").appendChild(h1);
+
+// PAss more than one element
+
+// [
+//     <h1>City Name</h1>,
+//     <ul>
+//         <li>Karachi</li>
+//         <li>Lahore</li>
+//         <li>Islamabad</li>
+//     </ul>
+// ]
