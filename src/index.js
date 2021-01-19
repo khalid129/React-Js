2a3094174b59c4d7ab1b1650045d801c12db259d
Muhammad Khalid <muhammadkhalid.ch129@gmail.com>
2021-01-19 18:07:58 +0500

    Image gallery

--- src/index.js
+++ src/index.js
@@ -1,29 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css'
+const name = "Khalid";
+var img1 = "https://picsum.photos/200/300";
+var img2 = "https://picsum.photos/250/300";
+var img3 = "https://picsum.photos/300/300";
 
-ReactDOM.render(<React.Fragment>
-    <h1>City Name</h1>
-    <ul>
-        <li>Karachi</li>
-        <li>Lahore</li>
-        <li>Islamabad</li>
-    </ul>
-</React.Fragment>, document.getElementById('root'))
+ReactDOM.render(<>
+    <h1 className="heading">My name is {name}</h1>
+    <div className="img_div">
+    <img src={img1} alt="image is here"/>
+    <img src={img2} alt="image is here"/>
+    <img src={img3} alt="image is here"/>
+    </div>
+</>
+    , document.getElementById("root"));
 
-
-// Alternative of React Element
-
-// var h1 = document.createElement("h1");
-// h1.innerHTML = "Hello World";
-// document.getElementById("root").appendChild(h1);
-
-// PAss more than one element
-
-// [
-//     <h1>City Name</h1>,
-//     <ul>
-//         <li>Karachi</li>
-//         <li>Lahore</li>
-//         <li>Islamabad</li>
-//     </ul>
-// ]
