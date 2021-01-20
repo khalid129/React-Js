b1c2223f4bc23885a13635b1865e7efac7e2776d
Muhammad Khalid <muhammadkhalid.ch129@gmail.com>
2021-01-20 23:54:47 +0500

    Greeting web with inline css

---
+++ src/image.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './image.css';
+
+const name = "Khalid";
+var img1 = "https://picsum.photos/200/300";
+var img2 = "https://picsum.photos/250/300";
+var img3 = "https://picsum.photos/300/300";
+const heading = {
+    color: "#ff6765",
+    textAlign: "center",
+    textTansform: 'capitalize',
+    fontweight: 'bold',
+    fontsize: 'large',
+    margin: '50px 0px',
+    fontfamily: "'Open Sans','sans serif'"
+};
+ReactDOM.render(<>
+    <h1 className="heading" style={heading}>My name is {name}</h1>
+    <div className="img_div">
+        <img src={img1} alt="Content is here" />
+        <img src={img2} alt="Content is here" />
+        <img src={img3} alt="Content is here" />
+    </div>
+</>
+    , document.getElementById("root"));
+
--- src/index.js
+++ src/index.js
@@ -1,18 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css'
-const name = "Khalid";
-var img1 = "https://picsum.photos/200/300";
-var img2 = "https://picsum.photos/250/300";
-var img3 = "https://picsum.photos/300/300";
+
+let currentDate = new Date(2021,1,20, 5);
+let curDate = currentDate.getHours();
+var greeting = '';
+let cssColor = {};
+let greet = {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    margin: '250px auto', 
+    width: '500px',
+    height: '50px',
+    borderRadius: '25px',
+    backgroundColor: '#fab1a0' 
+}
+if(curDate>=5 && curDate <=12){
+    greeting = 'Good Morning';
+    cssColor.color = "#e74c3c";
+}
+else if(curDate >12 && curDate<=17){
+    greeting = 'Good Afternoon';
+    cssColor.color = "#27ae60";
+}
+else if (curDate>18 && curDate<=21){
+    greeting = 'Good Evening';
+    cssColor.color = "#3498db";
+}
+else{
+    greeting = 'Good Night';
+    cssColor.color = "#8e44ad";
+}
 
 ReactDOM.render(<>
-    <h1 className="heading">My name is {name}</h1>
-    <div className="img_div">
-    <img src={img1} alt="Content is here"/>
-    <img src={img2} alt="Content is here"/>
-    <img src={img3} alt="Content is here"/>
-    </div>
-</>
-    , document.getElementById("root"));
-
+<div style= {greet}>
+<h1>Asalam Alaikum <span style = {cssColor}>{greeting}</span></h1>
+</div>
+</>,document.getElementById("root"));
