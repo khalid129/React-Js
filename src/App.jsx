afb3e0ae075eb63ca34ef04a4a682e4f02c73441
Muhammad Khalid <muhammadkhalid.ch129@gmail.com>
2021-01-25 19:43:22 +0500

    Complete React component with Css

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import Heading from './Heading';
 import Info from './Info';
 
 function App() {
     return (
         <>
-            <Heading />
-            <Info />
+            <Info/>
         </>
     );
 }
--- src/Heading.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function Heading() {
-    return <h1>Citites of the Pakistan</h1>
-}
-
-export default Heading;
